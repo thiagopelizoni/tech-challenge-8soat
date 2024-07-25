1a2a17fcd097b9e28389ba95561203c119e56fd1
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-07-25 18:07:28 -0300

    Testes unitários básicos da model Categoria

--- api/test/models/categoria_test.rb
+++ api/test/models/categoria_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class CategoriaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @categoria = Categoria.new(nome: "Lanches")
+  end
+
+  test "deve ser válida" do
+    assert @categoria.valid?
+  end
+
+  test "nome deve estar presente" do
+    @categoria.nome = ""
+    assert_not @categoria.valid?
+  end
+
+  test "nome deve ser único" do
+    duplicate_categoria = @categoria.dup
+    @categoria.save
+    assert_not duplicate_categoria.valid?
+  end
 end
