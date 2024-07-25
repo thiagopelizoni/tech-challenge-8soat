dfc81f2e3b98406ebe8093498bbd06b0036565f2
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-07-25 17:57:26 -0300

    Seeds de Categoria

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Categoria.create([
+  { nome: 'Lanches' },
+  { nome: 'Acompanhamentos' },
+  { nome: 'Bebidas' },
+  { nome: 'Sobremesas' }
+])
