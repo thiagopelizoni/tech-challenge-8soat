e544eaf47e88d4fbf04f43cef680b60e9fe30494
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-07-27 02:29:51 -0300

    DateValidator para Cliente

--- api/app/models/cliente.rb
+++ api/app/models/cliente.rb
@@ -2,7 +2,7 @@ class Cliente < ApplicationRecord
   has_secure_password validations: false
 
   validates :nome, presence: true, length: { minimum: 6, maximum: 140 }
-  validates :data_nascimento, presence: true, date: { before_or_equal_to: 16.years.ago, message: 'deve ser de no mínimo 16 anos de idade' }
+  validates :data_nascimento, presence: true, date: true
   validates :cpf, presence: true, uniqueness: true, format: { with: /\A\d{11}\z/, message: "deve conter 11 dígitos" }
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
   validates :senha, presence: true, length: { minimum: 6 }
