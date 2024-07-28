f23b543a46b947157c95a4cbd6237e88c153aa4a
Thiago Pelizoni <tpelizoni@fcl.com.br>
2024-07-28 15:49:50 -0300

    Data e Data de status sendo exibida

--- api/app/serializers/pedido_serializer.rb
+++ api/app/serializers/pedido_serializer.rb
@@ -1,9 +1,17 @@
 class PedidoSerializer < ActiveModel::Serializer
-  attributes :id, :valor, :status, :observacao
+  attributes :id, :valor, :status, :observacao, :data, :data_status
   belongs_to :cliente, serializer: ClienteSerializer
   has_many :produtos, serializer: ProdutoSerializer
 
   def produtos
     object.produtos.map { |produto_id| Produto.find(produto_id) }
+  end
+
+  def data
+    object.created_at.strftime("%d/%m/%Y %H:%m:%S")
+  end
+
+  def data_status
+    object.updated_at.strftime("%d/%m/%Y %H:%m:%S")
   end
 end
