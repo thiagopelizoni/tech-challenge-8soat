22068c237d290ead3895ed01a176defa6a2b1ad0
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2024-10-10 19:55:03 -0300

    Pequenos ajustes

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs)
-
-build_args=$(grep -v '^#' .env | sed 's/^/--build-arg /' | xargs)
-
 docker build $build_args -t thiagopelizoni/fastfood-api:latest .
+
+docker push thiagopelizoni/fastfood-api:latest
