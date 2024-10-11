2848e21ed167af05ce888b990b89fb5fd0dd8860
Thiago Pelizoni <thiago.pelizoni@gmail.com>
2024-10-11 18:24:26 -0300

    Atualizando arquivos de configuracao do Kubernetes

--- api/lib/db_setup.rb
+++ api/lib/db_setup.rb
@@ -1,15 +1,15 @@
 #!/usr/bin/env ruby
 
 def check_db_initialized
-    system("rails db:version > /dev/null 2>&1")
+    system("bin/rails db:version > /dev/null 2>&1")
 end
   
 if check_db_initialized
-    system("rails db:migrate")
+    system("bin/rails db:migrate")
 else
     puts "Inicializando banco de dados"
-    system("rails db:create")
-    system("rails db:migrate")
-    system("rails db:seed")
+    system("bin/rails db:create")
+    system("bin/rails db:migrate")
+    system("bin/rails db:seed")
 end
   
--- apply.sh
+++ apply.sh
@@ -2,7 +2,7 @@
 
 kubectl apply -f kubernetes/secret.yml
 kubectl apply -f kubernetes/api-pvc.yml
-kubectl apply -f kubernetes/postgres-pvc.yml
+kubectl apply -f kubernetes/db-pvc.yml
 kubectl apply -f kubernetes/db-deployment.yml
 kubectl apply -f kubernetes/db-service.yml
 kubectl apply -f kubernetes/api-deployment.yml
